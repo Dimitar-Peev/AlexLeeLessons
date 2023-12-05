4e6ddc6496490a66960722b3d0cd63039c18ce4f
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-05 18:20:51 +0200

    Create Demo_Array.java

---
+++ src/Demo_Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Demo_Array {
+    public static void main(String[] args) {
+
+        String[] fruits = new String[4];
+
+        fruits[0] = "Mango";
+        fruits[1] = "Apple";
+        fruits[2] = "Strawberry";
+        fruits[3] = "Watermelon";
+
+        System.out.println(Arrays.toString(fruits));
+    }
+}
